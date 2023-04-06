5b26a35c4a4dc8ae7b17445358ff967d2f3ee97c
unknown <lourencomonica318@gmail.com>
2023-04-06 18:28:27 +0100

    add new featurs in my debit card

--- 13/function.js
+++ 13/function.js
@@ -1,3 +1,28 @@
-IMask(document.querySelector("#cc-cvv"), {
-  mask: "0000",
+IMask(documnet.querySelector('#cc-cv'),{
+  mask:'0000'
+}
+)
+
+IMask(document.querySelector('#cc-number'),{
+  mask:'0000 0000 0000 0000'
+}
+)
+
+IMask(document.querySelector("cc-validity"), {
+  mask: "MM{/}YY",
+  blocks: {
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12,
+    },
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear()+18).slice(2),
+    },
+  },
 })
+
+
+  
